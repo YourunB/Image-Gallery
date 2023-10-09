54e45f98096201c9ba54fe3be4fd6b65d13ab087
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-10-09 09:50:20 +0300

    feat: add keypress for work and fillwindow images

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -12,6 +12,72 @@ const btnPrev = document.getElementById('btn-prev');
 const btnNext = document.getElementById('btn-next');
 const btnUp = document.getElementById('btn-up');
 
+let page = 1;
+let search = 'all';
+
+inputSearch.addEventListener('keypress', () => {
+  if (event.key === 'Enter' && inputSearch.value.length > 0) {
+    clearCards();
+    page = 1;
+    search = inputSearch.value;
+    getImages(page, search);
+    page += 1;
+    setTimeout(() => { fillWindow(); }, 250);
+  }
+});
+
+btnSearch.addEventListener('click', () => {
+  if (inputSearch.value.length > 0) {
+    clearCards();
+    page = 1;
+    search = inputSearch.value;
+    getImages(page, search);
+    setTimeout(() => { fillWindow(); }, 250);
+  }
+});
+
+inputSearch.addEventListener('input', () => {
+  if (inputSearch.value === '' || inputSearch.value.length === 0) {
+    clearSearch.classList.remove('header__clear-btn-active');
+    btnSearch.classList.remove('header__search-btn-active');
+    clearCards();
+    page = 1;
+    search = 'all';
+    getImages(page, search);
+    setTimeout(() => { fillWindow(); }, 250);
+  } else {
+    clearSearch.classList.add('header__clear-btn-active');
+    btnSearch.classList.add('header__search-btn-active');
+  }
+});
+
+clearSearch.addEventListener('click', () => {
+  if (inputSearch.value.length > 0) {
+    clearSearch.classList.remove('header__clear-btn-active');
+    btnSearch.classList.remove('header__search-btn-active');
+    inputSearch.value = '';
+    clearCards();
+    page = 1;
+    search = 'all';
+    getImages(page, search);
+    setTimeout(() => { fillWindow(); }, 250);
+  }
+});
+
+function fillWindow() {
+  if (document.body.getBoundingClientRect().height <= window.screen.height) {
+    page += 1;
+    getImages(page, search);
+  }
+}
+
+function clearCards() {
+  let cards = document.getElementsByClassName('cards');
+  for (let i = cards.length - 1; i >= 0; i -= 1) {
+    cards[i].remove();
+  }
+}
+
 getImages(page, search);
 function getImages(page, search = 'all') {
   let url = '';
@@ -92,6 +158,9 @@ window.addEventListener("scroll", throttle( () => {
     setTimeout(() => { getImages(page, search); }, 250);
   }
 }, 250));
+
+let images;
+let count = 0;
 
 document.body.addEventListener('click', () => {
   audioClick.play();
@@ -210,3 +279,5 @@ function handleGesture() {
     },2000);
   }
 }
+
+setTimeout(() => { fillWindow(); }, 250);
